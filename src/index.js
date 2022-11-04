24c8a941894519910a482c9690240e75ffa2d091
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-11-04 11:12:16 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+
+// https://restcountries.com/v3.1/name/peru
+// https://restcountries.com/v3.1/name/{name}
+
+
+fetch('https://restcountries.com/v3.1/name/peru').then(data => console.log(data.json()));
+
+
