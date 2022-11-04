dcf4f9a71142f1e4c5b5d7531f4d900337e6bcbc
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-11-04 09:12:41 +0000

    Deploying to gh-pages from @ SergY29/goit-js-hw-10@24c8a941894519910a482c9690240e75ffa2d091 🚀

---
+++ index.ace68305.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/peru").then((e=>console.log(e.json())));
+//# sourceMappingURL=index.ace68305.js.map
---
+++ index.e579be12.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/peru").then((function(e){return console.log(e.json())}));
+//# sourceMappingURL=index.e579be12.js.map
