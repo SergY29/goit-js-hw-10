6c7baaf9a90bca1b766c06d6eebe318fa6ba94b3
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-11-05 11:00:14 +0200

    preFinal

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,7 +3,12 @@ const URL = 'https://restcountries.com/v3.1'
 
 
 function fetchCountries(name) {
-    return fetch(`${URL}/name/${name}?fields=name,capital,population,flags,languages`).then(data => data.json());
+    return fetch(`${URL}/name/${name}?fields=name,capital,population,flags,languages`).then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    });
 };
 
 export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -19,14 +19,42 @@ function onSearch(e) {
     e.preventDefault();
     let value = e.target.value.trim();
 
+    if (value === '') {
+        return;
+    };
 
-    // fetchCountries(value.trim())
+    fetchCountries(value).then(result => {
+        if (result.length > 10) {
+            Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
+            return;
+        }
+        else if (result.length === 1) {
+            Notiflix.Notify.info('один');
+            return;
+        }
+        else {
+            Notiflix.Notify.info('все ок.');
+            return;
+
+        };
+    })
+        .catch(error => {
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+        });
+
 
     console.log(fetchCountries(value));
 
 
 }
 
+function markupAll() {
+
+};
+
+function markupOne() {
+
+};
 
 
 
